93316a7b6488005a28e7ad86319e5162a9424a67
MKan <kanabaj.maciej@gmail.com>
2023-08-02 22:48:17 +0200

    Init commit

---
+++ src/main/java/pl/zajavka/project_manager/ProjectManagerApiApplication.java
@@ -0,0 +1,13 @@
+package pl.zajavka.project_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/zajavka/project_manager/ProjectManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zajavka.project_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
